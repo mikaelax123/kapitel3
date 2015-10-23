887bb24146e50b10c8f66c43d1cd3b0fa69b42b5
mikaelax123 <mikaelax123@gmail.com>
2015-10-23 09:32:30 +0200

    sten

--- ovning.php
+++ ovning.php
@@ -1,13 +1,26 @@
 <!DOCTYPE html>
 <html lang="sv">
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-        <link rel="stylesheet" href="style.css">
-        <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-        <script src="script.js"></script>
-    </head>
-    <body>
 
-    </body>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="script.js"></script>
+</head>
+
+<body>
+    <?php
+    $tal1 = 30;
+    $tal2 = 13;
+
+    $summa = $tal1 + $tal2;
+    $produkt = $tal1 * $tal2;
+    $sub = $tal1 - $tal2;
+    $division = $tal1 / $tal2;
+    $modulus = $tal1 % $tal2
+    echo $modulus;
+    ?>
+</body>
+
 </html>
