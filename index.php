ad77f1a57f8e13b13e3d39ea8b7d0893c260ee3e
mikaelax123 <mikaelax123@gmail.com>
2015-10-23 08:58:34 +0200

    sten

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="sv">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="style.css">
+        <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+        <script src="script.js"></script>
+    </head>
+    <body>
+
+    </body>
+</html>
